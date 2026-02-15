d9da4b3d58ba6611e49fd2fca4acf5bf197859e7
yiwang <468731422@qq.com>
2026-02-16 02:44:42 +0800

    add-markdown-renderer-module-with-tests

--- src/views/aiChatView.ts
+++ src/views/aiChatView.ts
@@ -300,6 +300,9 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
         }
         
         function renderMarkdown(text) {
+            if (!text) {
+                return '<p></p>';
+            }
             let html = esc(text);
             
             var backtick = String.fromCharCode(96);
@@ -312,19 +315,25 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
             html = html.replace(inlineCodeRegex, '<code>$1</code>');
             
             html = html.replace(/\\*\\*([^*]+)\\*\\*/g, '<strong>$1</strong>');
-            html = html.replace(/\\*([^*]+)\\*/g, '<em>$1</em>');
             
             html = html.replace(/^### (.+)$/gm, '<h3>$1</h3>');
             html = html.replace(/^## (.+)$/gm, '<h2>$1</h2>');
             html = html.replace(/^# (.+)$/gm, '<h1>$1</h1>');
             
-            html = html.replace(/^> (.+)$/gm, '<blockquote>$1</blockquote>');
+            html = html.replace(/^&gt; (.+)$/gm, '<blockquote>$1</blockquote>');
             
-            html = html.replace(/^[-*] (.+)$/gm, '<li>$1</li>');
+            html = html.replace(/^[-] (.+)$/gm, '<li>$1</li>');
+            html = html.replace(/^[*] (.+)$/gm, '<li>$1</li>');
             html = html.replace(/(<li>.*<\\/li>\\n?)+/g, '<ul>$&</ul>');
             
-            var linkRegex = /\\[([^\\]]+)\\]\\(([^)]+)\\)/g;
-            html = html.replace(linkRegex, '<a href="$2">$1</a>');
+            html = html.replace(/\\*([^*\\n]+?)\\*/g, function(match, content) {
+                if (content.startsWith(' ') || content.endsWith(' ')) {
+                    return match;
+                }
+                return '<em>' + content + '</em>';
+            });
+            
+            html = html.replace(/\\[([^\\]]+)\\]\\(([^)]+)\\)/g, '<a href="$2">$1</a>');
             
             html = html.replace(/^---$/gm, '<hr>');
             
