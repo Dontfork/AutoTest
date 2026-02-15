a2d6169060a70050f6978c05cc85820001b13568
yiwang <468731422@qq.com>
2026-02-16 02:33:17 +0800

    fix-markdown-regex-escaping-in-template-string

--- src/views/aiChatView.ts
+++ src/views/aiChatView.ts
@@ -303,7 +303,7 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
             let html = esc(text);
             
             var backtick = String.fromCharCode(96);
-            var codeBlockRegex = new RegExp(backtick + backtick + backtick + '(\\w*)\\n([\\s\\S]*?)' + backtick + backtick + backtick, 'g');
+            var codeBlockRegex = new RegExp(backtick + backtick + backtick + '(\\\\w*)\\\\n([\\\\s\\\\S]*?)' + backtick + backtick + backtick, 'g');
             html = html.replace(codeBlockRegex, function(match, lang, code) {
                 return '<pre><code class="language-' + lang + '">' + code.trim() + '</code></pre>';
             });
@@ -311,8 +311,8 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
             var inlineCodeRegex = new RegExp(backtick + '([^' + backtick + ']*)' + backtick, 'g');
             html = html.replace(inlineCodeRegex, '<code>$1</code>');
             
-            html = html.replace(/\\*\\*([^\\*]+)\\*\\*/g, '<strong>$1</strong>');
-            html = html.replace(/\\*([^\\*]+)\\*/g, '<em>$1</em>');
+            html = html.replace(/\\*\\*([^*]+)\\*\\*/g, '<strong>$1</strong>');
+            html = html.replace(/\\*([^*]+)\\*/g, '<em>$1</em>');
             
             html = html.replace(/^### (.+)$/gm, '<h3>$1</h3>');
             html = html.replace(/^## (.+)$/gm, '<h2>$1</h2>');
@@ -323,7 +323,8 @@ export class AIChatViewProvider implements vscode.WebviewViewProvider {
             html = html.replace(/^[-*] (.+)$/gm, '<li>$1</li>');
             html = html.replace(/(<li>.*<\\/li>\\n?)+/g, '<ul>$&</ul>');
             
-            html = html.replace(/\\[([^\\]]+)\\]\\(([^)]+)\\)/g, '<a href="$2">$1</a>');
+            var linkRegex = /\\[([^\\]]+)\\]\\(([^)]+)\\)/g;
+            html = html.replace(linkRegex, '<a href="$2">$1</a>');
             
             html = html.replace(/^---$/gm, '<hr>');
             
